7bdc01f874d36cc80d541f56d2286a7985c2e794
Akuseru0509 <nguyenminhkhoi2329@gmail.com>
2025-01-11 22:51:22 +0700

    Re-scale

--- script.js
+++ script.js
@@ -1,21 +1,2 @@
-let menu = document.querySelector('#menu-icon');
-let navlist = document.querySelector('.navlist');
-let box = document.querySelector('.box');
 
-menu.onclick = () => {
-    menu.classList.toggle('bx-x');
-    navlist.classList.toggle('open');
-}
 
-// document.querySelector('.scroll a').addEventListener('click', function(e) {
-//     e.preventDefault();
-//     const target = document.querySelector('#about');
-//     target.scrollIntoView({
-//         behavior: 'smooth',
-//         block: 'start'
-//     });
-// });
-
-document.querySelector('.banner').addEventListener('click', function() {
-    document.querySelector('.slider').classList.toggle('stop');
-});
